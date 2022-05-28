9caa6030ffc58284bcf43e27b58b16909a081a92
Jan <2002jan@gmail.com>
2022-05-28 10:26:58 +0200

    working kinda

--- EP.h
+++ EP.h
@@ -2,6 +2,12 @@
 #define EULERIAN_TOUR
 
 #include "AL.h"
+#include <stdio.h>
+
+void printEulerCycle();
+void findEulerCycle(int start);
+int8_t isValidEdge(int start, int v);
+int dfsCount(int v, int8_t *visited);
 
 void printEulerCycle()
 {
@@ -16,13 +22,64 @@ void printEulerCycle()
         }
     }
 
-
+    findEulerCycle(start);
 }
 
 void findEulerCycle(int start)
 {
-    int i, count = countEdges(start);
+    ALI *current = AdjacencyList[start];
 
+    while (current != NULL)
+    {
+        int target = current->j;
+
+        if (isValidEdge(start, target))
+        {
+            printf("%d -> %d, ", start, target);
+
+            removeUDEdge(start, target);
+            findEulerCycle(target);
+        }
+
+        current = current->next;
+    }
+    
+}
+
+int8_t isValidEdge(int start, int v)
+{
+    if (countEdges(start) == 1)
+        return 1;
+
+    int8_t *visited = (int8_t *)calloc(AdjacencyListCount, sizeof(int8_t));
+    int c1 = dfsCount(start, visited);
+    free(visited);
+
+    removeUDEdge(start, v);
+    visited = (int8_t *)calloc(AdjacencyListCount, sizeof(int8_t));
+
+    int c2 = dfsCount(start, visited);
+    free(visited);
+
+    return (c1 > c2) ? 0 : 1;
+}
+
+int dfsCount(int v, int8_t *visited)
+{
+    visited[v] = 1;
+    int c = 1;
+
+    ALI *current = AdjacencyList[v];
+
+    while (current != NULL)
+    {
+        if (!visited[current->j])
+            c = c + dfsCount(current->j, visited);
+
+        current = current->next;
+    }
+
+    return c;
 }
 
 #endif
--- main.c
+++ main.c
@@ -1,10 +1,33 @@
 #include "graphGen.h"
+#include "EP.h"
 
 int main()
 {
     randomGraph(10, 0.5f);
 
     int i;
+
+    for(i = 0; i < AdjacencyListCount; i++)
+    {
+        ALI *start = AdjacencyList[i];
+
+        printf("%d -> ", i);
+
+        while (start != NULL)
+        {
+            printf("%d, ", start->j);
+
+            start = start->next;
+        }
+        printf("\n");
+        
+    }
+
+    printf("\n\n");
+
+    printEulerCycle();
+
+    printf("\n\n");
 
     for(i = 0; i < AdjacencyListCount; i++)
     {
